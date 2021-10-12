9aaa9c719a7f441e58a635bd37595938fbaa7453
viniciusroratto <viniciusroratto@gmail.com>
2021-10-12 15:59:09 -0300

    V1 - Nao funcional

--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -1,5 +1,48 @@
+import java.lang.System;
+import javax.websocket.server.ServerEndpoint;
+import javax.websocket.Session; 
+import javax.websocket.CloseReason;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.OnClose;
+import java.util.logging.Logger;
+import java.io.IOException;
+import java.util.*;
 
+@ServerEndpoint("/chat")
 public class ChatServer{
 
+	private static Set<Session> conversas;
+
+	@OnOpen
+	public void OnOpen (Session session){
+		conversas.add(session);
+		String sessionId = session.getId();
+		System.out.println(String.format("Usuario %s entrou no chat", sessionId));
+		// adicionar dados extras.
+	}
+
+	@OnMessage
+	public void OnMessage (String mensagem) throws IOException{
+
+		try{
+			for(Session conversa: conversas){
+				conversa.getBasicRemote().sendText(mensagem);
+			}
+		}
+		
+		catch (IOException e){
+			System.out.println(String.format("Erro no envio da mensagem: \"%s\"", mensagem));
+		}	
+	}
+
+	@OnClose
+	public void OnClose (Session session, CloseReason motivo){
+		String sessionId = session.getId();
+		System.out.println(String.format("Usuario %s saiu do chat", sessionId));
+		System.out.println(String.format("%s", motivo.getReasonPhrase().toString()));
+		conversas.remove(session);
+		
+	}
 
 }
